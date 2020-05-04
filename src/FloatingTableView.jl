eac088b1b70d28327090599a6079a5310bb54eb3
pdeffebach <p.deffebach@gmail.com>
2020-05-04 16:17:36 -0400

    make default window permanent for improved speed

--- src/FloatingTableView.jl
+++ src/FloatingTableView.jl
@@ -4,9 +4,35 @@ using Blink, TableView
 
 export browse
 
+mutable struct CurrentWin
+    nullablewin::Union{Blink.Window, Nothing}
+end
+const CURRENT_WIN = CurrentWin(nothing)
+
+iswinnull() = CURRENT_WIN.nullablewin === nothing
+
+"""
+    current()
+Returns the Blink Window object for the current plot
+"""
+function current()
+    if iswinnull()
+        error("No current window")
+    end
+    CURRENT_WIN.nullablewin
+end
+
+current(win::Blink.Window) = (CURRENT_WIN.nullablewin = win)
+
 function browse(df)
-    w =Blink.Window()
-    body!(w, showtable(df))
+	if iswinnull() || !active(current())
+		w = Blink.Window()
+		current(w)
+		body!(current(), showtable(df))
+	else
+		body!(current(), showtable(df))
+	end
+
     return nothing
 end
 

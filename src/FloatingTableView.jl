d03318bb3140803826cf5a881a28d893885604ac
pdeffebach <p.deffebach@gmail.com>
2020-07-19 15:15:46 -0400

    more docstring

--- src/FloatingTableView.jl
+++ src/FloatingTableView.jl
@@ -25,10 +25,34 @@ end
 current(win::Blink.Window) = (CURRENT_WIN.nullablewin = win)
 
 """
-	browse(df; kwargs)
+	browse(t; kwargs)
 
-Browse a Tables.jl compatible data source in a new Blink window using 
+Browse data source in a new Blink window using 
 Tables.jl's `showtable` function. 
+
+# Arguments
+
+- `t`, a Tables.jl-compatible data source, 
+  for example a `DataFrame` or a `NamedTuple` 
+  of `Vector`s
+- `kwargs`, keyword arguments passed to the 
+  `showtable` command from TableView.jl to 
+  control the details of the new data viewer.
+  See [`showtable`](@ref) for details.
+
+# Examples
+
+```julia
+julia> t = (a = rand(10), b = rand(10))
+
+julia> browse(t)
+```
+
+```julia
+julia> t = rand(100, 100)
+
+julia> browse(t, dark = true)
+```
 
 See also: [`showtable`](@ref)
 """

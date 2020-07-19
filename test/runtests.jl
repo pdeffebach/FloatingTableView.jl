09155756812c727d827ee7770fe8afb6306d4ca6
pdeffebach <p.deffebach@gmail.com>
2020-07-19 15:43:27 -0400

    add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,4 +4,8 @@ using Test
 @testset "FloatingTableView.jl" begin
     t = (a = [1], b = [2])
     browse(t)
+
+    browse(t, dark = true)
+    browse(t, width = "50%")
+    browse(t, height = 600)
 end
